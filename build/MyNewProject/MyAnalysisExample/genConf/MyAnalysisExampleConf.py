f7e57a480987ea338cdd81f4109c33d9b5e12879
Sota Abe <abes@login01.icepp.jp>
2019-04-18 14:45:54 +0900

    change calculation method of invariant mass, and it seems that peak exists at 90GeV, but 1 more peak eists at low mass region

--- build/MyNewProject/MyAnalysisExample/genConf/MyAnalysisExampleConf.py
+++ build/MyNewProject/MyAnalysisExample/genConf/MyAnalysisExampleConf.py
@@ -1,4 +1,4 @@
-#Thu Apr 18 14:23:43 2019"""Automatically generated. DO NOT EDIT please"""
+#Thu Apr 18 14:38:07 2019"""Automatically generated. DO NOT EDIT please"""
 from GaudiKernel.GaudiHandles import *
 from AthenaCommon.Configurable import *
 
